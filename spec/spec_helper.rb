b9fe8ef00bb0f73b6d940dd45ff3cd3a7a351d5d
Joe Fredette <jfredett@gmail.com>
2012-07-04 09:41:34 -0400

    add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'bundler/setup'
+
+require './initializers/initialize'
+require 'pry'
+
+RSpec.configure do |config|
+  config.before do
+    allow_message_expectations_on_nil #because we're making proper nullclasses.
+  end
+end
+
+#include helpers
+Dir["./spec/helpers/*.rb"].each { |file| require file }
