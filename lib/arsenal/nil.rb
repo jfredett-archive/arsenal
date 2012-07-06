7afd9e22a1e1baa6a7e3a09f97b64b16992c0b64
Joe Fredette <jfredett@gmail.com>
2012-07-06 09:50:05 -0400

    extract Nil AS::Concern into two other modules, Arsenal::{NilMacros,NilModel}

--- lib/arsenal/nil.rb
+++ lib/arsenal/nil.rb
@@ -1,4 +1,33 @@
 module Arsenal
+  module NilModel
+    def nil?       ; true  ; end
+    def persisted? ; false ; end
+    def savable?   ; false ; end
+    def id         ; nil   ; end
+
+    def attributes ; {id: nil}    ; end
+
+    def nil_model
+      self.class.nil_model
+    end
+
+    def method_missing(method, *args, &block) 
+      return super if respond_to? method
+
+      if attr = nil_model.attributes[method]
+        return attr.default
+      end
+
+      super
+    end
+  end
+
+  module NilMacros
+    attr_accessor :nil_model
+  end
+
+
+  # @private
   module Nil
     extend ActiveSupport::Concern
 
@@ -7,30 +36,11 @@ module Arsenal
     end
 
     module InstanceMethods
-      def nil?       ; true  ; end
-      def persisted? ; false ; end
-      def savable?   ; false ; end
-      def id         ; nil   ; end
-
-      def attributes ; {id: nil}    ; end
-
-      def nil_model
-        self.class.nil_model
-      end
-
-      def method_missing(method, *args, &block) 
-        return super if respond_to? method
-
-        if attr = nil_model.attributes[method]
-          return attr.default
-        end
-
-        super
-      end
+      include Arsenal::NilModel
     end
 
     module ClassMethods
-      attr_accessor :nil_model
+      include Arsenal::NilMacros
     end
   end
 end
