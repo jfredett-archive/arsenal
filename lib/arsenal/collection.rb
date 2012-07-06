6967e1c9de35c7ef08ffd85a49e1a627c84277a3
Joe Fredette <jfredett@gmail.com>
2012-07-06 12:10:03 -0400

    drop AS::Concern from Arsenal::Collection

--- lib/arsenal/collection.rb
+++ lib/arsenal/collection.rb
@@ -1,29 +1,25 @@
 module Arsenal
   module Collection
-    extend ActiveSupport::Concern
+    def savable?
+      all_savable?
+    end
 
-    module InstanceMethods
-      def savable?
-        all_savable?
+    def persisted?
+      all_persisted?
+    end
+
+    def method_missing(method, *args, &block)
+      super if respond_to? method 
+
+      iterator = :each
+      predicate = method
+
+      if method =~ /(any|all)_(.*\?)/
+        iterator = "#{$1}?".to_sym
+        predicate = $2.to_sym
       end
 
-      def persisted?
-        all_persisted?
-      end
-
-      def method_missing(method, *args, &block)
-        super if respond_to? method 
-
-        iterator = :each
-        predicate = method
-
-        if method =~ /(any|all)_(.*\?)/
-          iterator = "#{$1}?".to_sym
-          predicate = $2.to_sym
-        end
-
-        send(iterator) { |e| e.send(predicate, *args, &block) } 
-      end
+      send(iterator) { |e| e.send(predicate, *args, &block) } 
     end
   end
 end
