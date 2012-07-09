39a977201eba21d8af778a47760e1e34f3cf41c2
Joe Fredette <jfredett@gmail.com>
2012-07-09 10:31:19 -0400

    extract Arsenal::Model::Collection spec

--- spec/arsenal_spec.rb
+++ spec/arsenal_spec.rb
@@ -55,63 +55,6 @@ describe "The Arsenal Module" do
         "Example::Collection".should be_defined
       end
     end
-    describe 'Example::Persisted' do
-      context 'class' do
-        subject { Example::Persisted } 
-
-        its(:superclass) { should be Example }
-      end
-
-      context 'instance' do
-        subject { Example::Persisted.new } 
-
-        it { should respond_to :persisted? }
-        it { should be_persisted }
-
-        it { should respond_to :savable? }
-        it { should be_savable } 
-
-        it { should respond_to :nil? }
-        it { should_not be_nil }
-      end
-    end
-
-    describe 'Example::Collection' do
-      context 'instance' do 
-        subject { Example::Collection.new([Example.new, Example.new, Example::Persisted.new, nil_example]) }
-
-        it { should respond_to :each } 
-
-        it "delegates calls it doesn't understand to it's elements" do
-          subject.each { |e| e.should_receive(:foo) } 
-          subject.foo
-        end
-
-        it "takes methods of the form #any_<predicate>? and turns them into a .any? call" do
-          subject.each { |e| e.should_receive(:predicate?).at_most(:once) } 
-          subject.should_receive(:any?)
-          subject.any_predicate?
-        end
-
-        it "takes methods of the form #all_<predicate>? and turns them into a .all? call" do
-          subject.each { |e| e.should_receive(:predicate?).at_most(:once) } 
-          subject.should_receive(:all?)
-          subject.all_predicate?
-        end
-
-        it "aliases #savable? to #all_savable?" do
-          subject.each { |e| e.should_receive(:savable?).at_most(:once) } 
-          subject.should_receive(:all?)
-          subject.savable?
-        end
-
-        it "aliases #persisted? to #all_persisted?" do
-          subject.each { |e| e.should_receive(:persisted?).at_most(:once) } 
-          subject.should_receive(:all?)
-          subject.savable?
-        end
-      end
-    end
 
     describe 'Example::Nil', 'instance' do
       before {
---
+++ spec/collection_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe 'Example::Collection' do
+  before { 
+    class Example
+      include Arsenal 
+      id :identifier
+
+      def identifier
+        $identifier_number ||= 0
+        $identifier_number += 1
+      end
+    end 
+  }
+  after { Object.send(:remove_const, :Example) }
+
+  context 'instance' do 
+
+    subject { Example::Collection.new([Example.new, Example.new, Example::Persisted.new, nil_example]) }
+
+    it { should respond_to :each } 
+
+    it "delegates calls it doesn't understand to it's elements" do
+      subject.each { |e| e.should_receive(:foo) } 
+      subject.foo
+    end
+
+    it "takes methods of the form #any_<predicate>? and turns them into a .any? call" do
+      subject.each { |e| e.should_receive(:predicate?).at_most(:once) } 
+      subject.should_receive(:any?)
+      subject.any_predicate?
+    end
+
+    it "takes methods of the form #all_<predicate>? and turns them into a .all? call" do
+      subject.each { |e| e.should_receive(:predicate?).at_most(:once) } 
+      subject.should_receive(:all?)
+      subject.all_predicate?
+    end
+
+    it "aliases #savable? to #all_savable?" do
+      subject.each { |e| e.should_receive(:savable?).at_most(:once) } 
+      subject.should_receive(:all?)
+      subject.savable?
+    end
+
+    it "aliases #persisted? to #all_persisted?" do
+      subject.each { |e| e.should_receive(:persisted?).at_most(:once) } 
+      subject.should_receive(:all?)
+      subject.savable?
+    end
+  end
+end
+
