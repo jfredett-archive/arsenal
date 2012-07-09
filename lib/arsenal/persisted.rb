e248116dee42f749d3e903d4c6019b0f2c841ff8
Joe Fredette <jfredett@gmail.com>
2012-07-09 07:20:25 -0400

    document the persisted module

--- lib/arsenal/persisted.rb
+++ lib/arsenal/persisted.rb
@@ -1,5 +1,10 @@
 module Arsenal
+  # The Persisted module contains instance methods for the Perisited-model
+  # subclass of an arsenal model. 
   module Persisted
+    # By definition, a Persisted model is persisted.
+    #
+    # @return [Boolean] true
     def persisted? 
       true
     end
