9e727f84519d7b74a66a5a148e0f3eb8eb3ad22c
Joe Fredette <jfredett@gmail.com>
2012-07-09 07:17:39 -0400

    document the Collection module

--- lib/arsenal/collection.rb
+++ lib/arsenal/collection.rb
@@ -1,13 +1,42 @@
 module Arsenal
+  # The collection module contains instance methods for the Arsenal-model
+  # collection class. 
+  #
+  # By default, the collection-class for a model delegates all methods to it's
+  # components. When the call matches `(all|any)_<predicate>` (where
+  # `<predicate>` is any method ending in `?` and returning a Boolean), the
+  # result will be a Boolean which indicates if `all` (resp: `any`) of the
+  # elements assert the given predicate.
+  #
+  # In every case, this is overridden by a manually defined method on the class
+  # itself. So that if you define `all_foo?`, that method will be called instead
+  # of executing a `all?(&:foo?)` -- as would normally be the case.
   module Collection
+    # A collection is savable when it's elements are all savable.
+    #
+    # @see Arsenal::Persisted Arsenal::Persisted#savable?
+    # @see Arsenal::Nil Arsenal::Nil#savable?
+    # @see Arsenal::Collection Arsenal::Model#savable?
+    #
+    # @return [Boolean] true if all the elements assert `#savable?`, false
+    # otherwise
     def savable?
       all_savable?
     end
 
+    # A collection is persisted when it's elements are all persisted.
+    #
+    # @see Arsenal::Persisted Arsenal::Persisted#persisted?
+    # @see Arsenal::Nil Arsenal::Nil#persisted?
+    # @see Arsenal::Collection Arsenal::Model#persisted?
+    #
+    # @return [Boolean] true if all the elements assert `#savable?`, false
+    # otherwise
     def persisted?
       all_persisted?
     end
 
+    # @private
     def method_missing(method, *args, &block)
       super if respond_to? method 
 
