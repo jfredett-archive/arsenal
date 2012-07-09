c64618d5387deebf26809f0d91556fec631cd3a9
Joe Fredette <jfredett@gmail.com>
2012-07-08 23:58:57 -0400

    add docs for the nil-class implementation

--- lib/arsenal/nil.rb
+++ lib/arsenal/nil.rb
@@ -1,12 +1,44 @@
 module Arsenal
+  # The NilModel module contains instance methods for the Arsenal-model's
+  # Nil-class implementation.
+  #
+  # The Nil-class will also respond to the parent model's attributes, by name,
+  # as methods, and return the default value associated with them, if there is
+  # no default value, then `nil` is returned.
   module NilModel
+    
+    # The model should act-as-nil, so this is defined as true.
+    #
+    # @return [Boolean] always true
     def nil?       ; true  ; end
+
+    # This model is never persisted
+    #
+    # @return [Boolean] always false
     def persisted? ; false ; end
+    
+    # This model is not savable
+    #
+    # @return [Boolean] always false
     def savable?   ; false ; end
+
+    # This model has no id.
+    #
+    # @return nil
     def id         ; nil   ; end
 
+    # It returns the set of attributes corresponding to the set of default
+    # attributes on it's associated Model class. For each attribute which does
+    # not have a set default, it returns nil.
+    #
+    # @todo: Implement
+    #
+    # @return [Hash] All the attributes and their associated values for the
+    # model instance
     def attributes ; {id: nil}    ; end
 
+   
+    # @private
     def method_missing(method, *args, &block) 
       return super if respond_to? method
 
@@ -24,7 +56,10 @@ module Arsenal
     end
   end
 
+  # The NilMacros module loads class-level methods onto the Nil-class for a
+  # given Arsenal Model
   module NilMacros
+    # @private
     attr_accessor :nil_model
   end
 
