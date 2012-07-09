a5ddeb7e0bbe6efd8f44159323a6a7e443f92ad7
Joe Fredette <jfredett@gmail.com>
2012-07-09 10:34:45 -0400

    extract Arsenal::Model::Nil spec

--- spec/arsenal_spec.rb
+++ spec/arsenal_spec.rb
@@ -55,54 +55,6 @@ describe "The Arsenal Module" do
         "Example::Collection".should be_defined
       end
     end
-
-    describe 'Example::Nil', 'instance' do
-      before {
-        class Example
-          attribute :foo, :default => :bar
-          attribute :flurm, :default => :slurm
-        end
-
-        class Example::Nil
-          def flurm ; :thing ; end
-        end
-      }
-
-
-      subject { Example::Nil.instance } 
-
-      it { should respond_to :nil? }
-      it { should be_nil } 
-      it { should be_a Singleton } 
-
-      it { should respond_to :persisted? }
-      it { should_not be_persisted }
-
-      it { should respond_to :savable? }
-      it { should_not be_savable } 
-
-      it { should respond_to :attributes } 
-      it { should respond_to :id }
-      its(:id) { should be_nil }
-
-      # respects overrides of defaulted attribute methods
-      its(:flurm) { should == :thing }
-      # gives the attributes' default value it the attribute name is called as a method 
-      its(:foo) { should == :bar } 
-
-      describe "#attributes" do
-        subject { nil_example.attributes } 
-        its(:keys) { should include(:id) }
-        its(:keys) { should be_a_subset_of(Example.attributes.keys) } 
-      end
-
-      describe 'nil_<model_name>' do
-        subject { nil_example }
-
-        it { should be_the Example::Nil } 
-      end
-    end
-
     describe 'Example::Repository' do
       subject { Example::Repository } 
 
---
+++ spec/nil_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe 'Example::Nil', 'instance' do
+  before { 
+    class Example
+      include Arsenal 
+      id :identifier
+
+      attribute :foo, :default => :bar
+      attribute :flurm, :default => :slurm
+
+      def identifier
+        $identifier_number ||= 0
+        $identifier_number += 1
+      end
+    end 
+
+    class Example::Nil
+      def flurm ; :thing ; end
+    end
+  }
+  after { Object.send(:remove_const, :Example) }
+
+
+  subject { Example::Nil.instance } 
+
+  it { should respond_to :nil? }
+  it { should be_nil } 
+  it { should be_a Singleton } 
+
+  it { should respond_to :persisted? }
+  it { should_not be_persisted }
+
+  it { should respond_to :savable? }
+  it { should_not be_savable } 
+
+  it { should respond_to :attributes } 
+  it { should respond_to :id }
+  its(:id) { should be_nil }
+
+  # respects overrides of defaulted attribute methods
+  its(:flurm) { should == :thing }
+  # gives the attributes' default value it the attribute name is called as a method 
+  its(:foo) { should == :bar } 
+
+  describe "#attributes" do
+    subject { nil_example.attributes } 
+    its(:keys) { should include(:id) }
+    its(:keys) { should be_a_subset_of(Example.attributes.keys) } 
+  end
+
+  describe 'nil_<model_name>' do
+    subject { nil_example }
+
+    it { should be_the Example::Nil } 
+  end
+end
+
