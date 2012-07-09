11d661dc9350eba8f81135c200dccbe13881491a
Joe Fredette <jfredett@gmail.com>
2012-07-09 08:49:00 -0400

    document Arsenal::Repository

--- lib/arsenal/repository.rb
+++ lib/arsenal/repository.rb
@@ -1,13 +1,19 @@
 module Arsenal
+  # The Repository module contains methods for the Arsenal-model's associated
+  # repository. In particular, it interacts with drivers to serialize
+  # Arsenal-models into memory.
   module Repository
+    # todo: implement
     def save
 
     end
 
+    # todo: implement
     def find
 
     end
 
+    # todo: implement
     def destroy
 
     end
