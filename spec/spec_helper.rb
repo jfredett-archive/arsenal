a3e7258829bc5d699f9084c2f2b7ffe12d199f68
Joe Fredette <jfredett@gmail.com>
2012-07-10 14:35:02 -0400

    Ensure we load helpers first. Load DelegateMatcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,14 @@ require 'pry'
 
 require 'arsenal'
 
+#include helpers
+Dir["./spec/helpers/*.rb"].each { |file| require file }
+
 RSpec.configure do |config|
   config.before do
     allow_message_expectations_on_nil #because we're making proper nullclasses.
   end
+
+  config.include(CustomMatchers)
 end
 
-#include helpers
-Dir["./spec/helpers/*.rb"].each { |file| require file }
