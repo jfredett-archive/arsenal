cc387f5f9a380a5bbeca2e6d64c80f9adb0f2339
Joe Fredette <jfredett@gmail.com>
2012-07-16 08:39:57 -0400

    refactor Arsenal::AttributeCollection to use Set

--- lib/arsenal/attribute_collection.rb
+++ lib/arsenal/attribute_collection.rb
@@ -3,9 +3,7 @@ module Arsenal
   # of attributes.
   #
   # @see Arsenal::Attribute
-  class AttributeCollection
-    include Enumerable
-
+  class AttributeCollection < Set
     # Create a new collection of attributes
     #
     # @param arr [Array] a list of attributes
@@ -13,14 +11,14 @@ module Arsenal
     # @return [Arsenal::AttributeCollection] a new collection for the given
     #  attributes
     def initialize(arr = [])
-      @attrs = arr
+      super(arr)
     end
 
     # The set of attribute names
     #
     # @return an array of attribute names
     def keys
-      @attrs.map { |a| a.name } 
+      map { |a| a.name } 
     end
 
     # Add a new attribute to the collection
@@ -34,9 +32,8 @@ module Arsenal
     # @raise [ArgumentError] if the attribute-like object does not implement
     #  the required messages
     def <<(attr)
-      raise ArgumentError unless attr.respond_to? :name and attr.respond_to? :default
-      @attrs << attr
-      self
+      assert_quacks_like_attribute attr
+      super
     end
     alias push <<
 
@@ -47,10 +44,7 @@ module Arsenal
     # @return [Arsenal::Attribute, nil] Returns an attribute if one is found,
     #  nil otherwise
     def [](key)
-      @attrs.each do |a|
-        return a if a.name == key
-      end
-      nil
+      find { |e| e.name == key } 
     end
 
     # Given a model upon which to act, return a hash from attribute name to
@@ -61,20 +55,9 @@ module Arsenal
     # @return [Hash] a hash containing all of the attribute names as keys, 
     #  and their corresponding values as values
     def to_hash(obj)
-      @attrs.each.with_object({}) do |e,a|
+      each.with_object({}) do |e,a|
         a[e.name] = e.value(obj)
       end
-    end
-
-    # Execute the given block over each attribute
-    #
-    # @param block [#call] A block to execute on each attribute
-    #
-    # @return [Array] the list of attributes
-    #
-    # @todo bug here, should it return the AttributeCollection?
-    def each(&block)
-      @attrs.each(&block)
     end
 
     # Concatenate two AttributeCollections together.
@@ -86,27 +69,19 @@ module Arsenal
     #  containing the attributes from both collections
     def +(other)
       return self.dup if other.nil?
-
-      coll = AttributeCollection.new
-      each       { |attr| coll << attr } 
-      other.each { |attr| coll << attr } 
-      return coll
+      other.each { |attr| assert_quacks_like_attribute attr } 
+      super
     end
 
-    # Equality of AttributeCollections is determined by equal attribute-sets
-    #
-    # @param other [Arsenal::AttributeCollection] another attribute collection
-    #  to compare for equality
-    #
-    # @return [Boolean] true if the two collections have the same attributes,
-    #  false otherwise
-    def ==(other)
-      attrs = @attrs.dup
-      other.each do |attr|
-        return false unless attrs.include?(attr)
-        attrs.delete(attr)
-      end
-      attrs.empty? 
+    def for(driver)
+      self
+      #AttributeCollection.new(@attrs.select { |a| a.driver == driver || a.name == :id })
+    end
+
+    private 
+
+    def assert_quacks_like_attribute(attr)
+      raise ArgumentError unless attr.respond_to? :name and attr.respond_to? :default
     end
 
   end
