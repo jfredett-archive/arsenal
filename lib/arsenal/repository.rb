7eb6c9012fe8c1439fe884f83012325dcb880631
Joe Fredette <jfredett@gmail.com>
2012-07-16 07:36:41 -0400

    document Arsenal::Repository#save

--- lib/arsenal/repository.rb
+++ lib/arsenal/repository.rb
@@ -3,6 +3,16 @@ module Arsenal
   # repository. In particular, it interacts with drivers to serialize
   # Arsenal-models into memory.
   module Repository
+    # Serialize the given model to the backends it uses.
+    #
+    # **DOES NOT** necessarily operate transactionally. If you are serializing
+    # to multiple backends, you _may lose/corrupt data_ if something goes wrong
+    # with your hardware. _Always_ have a canonical, transactionally written
+    # serialization of each model, use transactional drivers, and _be careful_.
+    #
+    # @param model [Arsenal::Model] the model to serialize.
+    #
+    # @return [Boolean] true if the save was 100% successful, false otherwise.
     def save(model)
       return false         unless model.savable?
       return update(model) if model.persisted?
