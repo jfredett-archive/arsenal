c232b61db7b44725b0bfd1a23dd344426dc2add5
Joe Fredette <jfredett@gmail.com>
2012-08-21 08:00:32 -0400

    rework the arsenal main module

--- lib/arsenal.rb
+++ lib/arsenal.rb
@@ -1,67 +1,46 @@
 require "arsenal/version"
-require 'singleton'
-
 require 'initializer'
 
-require 'singleton'
-
 # Arsenal is a Repository-pattern focused ORM which supports lightweight
-# database drivers and polyglot persistence-by-default. It is unobtrusive,
+# database drivers and polyglot persistence by default. It is unobtrusive,
 # module based, and easy to use.
 module Arsenal
   extend ActiveSupport::Concern  
-  extend Forwardable
-  class << self
-    extend Forwardable 
-  end
 
   included do
     extend Forwardable
-
-    base = self
-
-    base::Repository = Class.new do
-      extend Arsenal::Repository
-    end
-    Arsenal.create_associated_model_method!(base::Repository, base)
-
-    base::Nil = Class.new do
-      include Arsenal::Nil
-    end
-    base::Nil.nil_model = base
-    Arsenal.create_associated_model_method!(base::Nil, base)
-
-    base::Persisted = Class.new(base) do
-      include Arsenal::Persisted
-    end
-    Arsenal.create_associated_model_method!(base::Persisted, base)
-
-    base::Collection = Class.new(Array) do
-      include Arsenal::Collection
-    end
-    Arsenal.create_associated_model_method!(base::Collection, base)
-
-    Arsenal.create_nil_method!(base)
-    Arsenal.register!(base)  
-
     extend Arsenal::Macros
+
     include Arsenal::Model
+
+    self::Repository    = Class.new        { extend  Arsenal::Repository }
+    self::Persisted     = Class.new(self)  { include Arsenal::Persisted  }
+    self::Collection    = Class.new(Array) { include Arsenal::Collection }
+    self::Nil           = Class.new        { include Arsenal::Nil        }
+
+    #TODO: get rid of this?
+    self::Nil.nil_model = self
+
+    Arsenal.create_associated_model_method!(self::Repository, self)
+    Arsenal.create_associated_model_method!(self::Nil, self)
+    Arsenal.create_associated_model_method!(self::Persisted, self)
+    Arsenal.create_associated_model_method!(self::Collection, self)
+
+    Arsenal.create_nil_method!(self)
+    
+    Arsenal.register!(self)  
   end
 
   class << self
-    delegate [:collection_for, :persisted_for] => :registry
-  end
+    extend Forwardable 
+    delegate [:register!, :collection_for, :persisted_for] => :registry
 
-  private 
-
-  class << self
-    delegate [:register!] => :registry
-
-    # the hash of all models which are wired up as arsenal models, and references
-    # to each of their arsenal-defined classes (including the model itself)
+    # The registry of all models which are wired up as arsenal models, and
+    # references to each of their arsenal-defined classes (including the model
+    # itself)
     #
     # @return [Arsenal::Registry] all of the Arsenal Models and their Arsenal-defined classes
-    def registry
+    def registry 
       @registry ||= Arsenal::ModelRegistry.new 
     end
 
--- lib/initializer.rb
+++ lib/initializer.rb
@@ -4,4 +4,7 @@ require 'active_support/core_ext/string/inflections'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/hash'
 
+require 'forwardable'
+require 'singleton'
+
 Dir['./lib/arsenal/**/*.rb'].each do |f| require f end
